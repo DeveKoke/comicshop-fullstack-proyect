70f0611e809bc10debc5c88245d98be53bb3a797
DeveKoke <jorge.be86@gmail.com>
2023-07-11 16:36:49 +0200

    No scraping

---
+++ models/comicsSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const comicSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    colection: {
+        type: String,
+        required: true,
+    },
+    character: {
+        type: String,
+        required: true,
+    },
+    pages: {
+        type: String,
+        required: true,
+    },
+    price:{
+        type:Number,
+        require: true,
+        },
+    description:{
+        type: String,
+        required: true,
+    },
+});
+
+const comic = new mongoose.model("comics", comicSchema);
+
+module.exports = comic;
+
--- utils/scrap.js
+++ utils/scrap.js
@@ -1,58 +1,58 @@
-const puppeteer = require("puppeteer");
-require('dotenv').config();
+// const puppeteer = require("puppeteer");
+// require('dotenv').config();
 
-const url = 'https://www.akiracomics.com/';
+// const url = 'https://www.akiracomics.com/';
 
-(async () => {
-    // Arrancamos pupeteer
-    const browser = await puppeteer.launch({
-        args: [
-          "--disable-setuid-sandbox",
-          "--no-sandbox",
-          "--single-process",
-          "--no-zygote",
-        ],
-        executablePath:
-          process.env.NODE_ENV === "production"
-              ? process.env.PUPPETEER_EXECUTABLE_PATH
-              : puppeteer.executablePath(),
-      });
+// (async () => {
+//     // Arrancamos pupeteer
+//     const browser = await puppeteer.launch({
+//         args: [
+//           "--disable-setuid-sandbox",
+//           "--no-sandbox",
+//           "--single-process",
+//           "--no-zygote",
+//         ],
+//         executablePath:
+//           process.env.NODE_ENV === "production"
+//               ? process.env.PUPPETEER_EXECUTABLE_PATH
+//               : puppeteer.executablePath(),
+//       });
 
-    // Abrimos nueva pagina
-    const page = await browser.newPage();
+//     // Abrimos nueva pagina
+//     const page = await browser.newPage();
 
-    // Navegar a la página deseada
-    await page.goto(url);
-    await page.waitForSelector('input[name=buscar]'); //espera a cargar el input
-    await page.click('.fa-search');  // click en botón
-    await page.waitForSelector('.buscar');  //espera a cargar selector con clase .buscar
-    await page.$eval('input[name=buscar]', el => el.value = 'spiderman'); 
-    await page. keyboard.press('Enter');
-    await page.waitForSelector('.producto');
+//     // Navegar a la página deseada
+//     await page.goto(url);
+//     await page.waitForSelector('input[name=buscar]'); //espera a cargar el input
+//     await page.click('.fa-search');  // click en botón
+//     await page.waitForSelector('.buscar');  //espera a cargar selector con clase .buscar
+//     await page.$eval('input[name=buscar]', el => el.value = 'spiderman'); 
+//     await page. keyboard.press('Enter');
+//     await page.waitForSelector('.producto');
 
 
-    const dataScrap = await page.$$eval('.producto', productContainer => {
-      return productContainer.map(product => {
-        const src = product.querySelector('div.imagen > a > img').getAttribute('src'); //url imagen
-        const title = product.querySelector('div.panel-informacion > h4').innerText;  //texto titulo
-        const link = product.querySelector('div.panel-informacion > h4 > a').getAttribute('href');  //link descripción
-        const price = product.querySelector('div.panel-informacion > div.panel.panel-precio > span.precio.ofertado').innerText;  //
+//     const dataScrap = await page.$$eval('.producto', productContainer => {
+//       return productContainer.slice(0, 15).map(product => {
+//         const src = product.querySelector('div.imagen > a > img').getAttribute('src'); //url imagen
+//         const title = product.querySelector('div.panel-informacion > h4').innerText;  //texto titulo
+//         const link = product.querySelector('div.panel-informacion > h4 > a').getAttribute('href');  //link descripción
+//         const price = product.querySelector('div.panel-informacion > div.panel.panel-precio > span.precio.ofertado').innerText;  //
         
-        const url = 'https://www.akiracomics.com';
-        const linkDescription = url+link;
-        const imgSrc = url+src;
+//         const url = 'https://www.akiracomics.com';
+//         const linkDescription = url+link;
+//         const imgSrc = url+src;
 
-        return {
-          imgSrc,
-          title,
-          linkDescription,
-          price
-        };
-      });
-    });
-  // Prefijo https://www.akiracomics.com/
-  console.log(dataScrap);
+//         return {
+//           imgSrc,
+//           title,
+//           linkDescription,
+//           price
+//         };
+//       });
+//     });
+//   // Prefijo https://www.akiracomics.com/
+//   console.log(dataScrap);
 
-  // Cerrar el navegador
-    await browser.close();
-  })();
+//   // Cerrar el navegador
+//     await browser.close();
+//   })();
